6f9a852c75de968d34bc39a82ed510a384690856
tuyenctbk <tuyenctbk@gmail.com>
2026-08-04 15:58:53 +0700

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.soloprono.pdfcraft"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "1.2.0"
+    versionCode = 5
+    versionName = "1.2.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
